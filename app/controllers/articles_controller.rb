6db7c3121bd365039af552bb341ef332605cf2e0
Pedro Santos <pedropaulo15@gmail.com>
2018-01-18 11:43:37 +0000

    Fix create, add flash and error messages, create show action and template.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,14 +5,23 @@ class ArticlesController < ApplicationController
   
   def create 
     # Works like a console.log, to print the content from the form
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      # Display a message to the user if the message was created.
+      flash[:notice] = "Article was successfully created!"
+      redirect_to article_path(@article)  
+    else
+      render 'new'
+    end
   end
   
   private def article_params
     params.require(:article).permit(:title, :description) 
+  end
+  
+  def show 
+    @article = Article.find(params[:id]);
   end
   
   
