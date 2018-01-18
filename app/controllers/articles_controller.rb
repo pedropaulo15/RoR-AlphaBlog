b318e1b6e56954ed9724d29aaa7708f945cd47eb
Pedro Santos <pedropaulo15@gmail.com>
2018-01-18 12:45:11 +0000

    Edit, Update Show and index listing.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,27 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    # @articles new instance variable is going to have all articles from the databse
+    @articles = Article.all
+  end
+  
   def new 
     @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
   
   def create 
