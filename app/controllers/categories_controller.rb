3799b1f64a0b24fa034588f22c9c3a04a5b1034d
Pedro Santos <pedropaulo15@gmail.com>
2018-03-10 20:37:52 +0000

    Update all views with categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,7 +22,8 @@ class CategoriesController < ApplicationController
   end
   
   def show
-    
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
   # Grab the data from the form, where a new category is inserted.
